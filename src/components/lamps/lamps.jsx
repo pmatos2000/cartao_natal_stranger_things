5c858080bf1bfc181b583b97799c2fee4d312a8b
Paulo <pmatos2000@gmail.com>
2020-12-04 21:37:36 -0300

    💡 novas sequencias para as lampadas

--- src/components/lamps/lamps.jsx
+++ src/components/lamps/lamps.jsx
@@ -3,59 +3,80 @@ import { SvgLoader, SvgProxy } from "react-svgmt";
 import lampImg from "./lamps.svg";
 import "./lamps.css"
 
-const idLamp = "ABCDEFGHIJKLMNOPQRSTUVXWYZ".split("");
+const idLamp = "RSTUVXWYZQPONMLKJABCDEFGHI".split("");
 
 const Lamps = () => {
     const [state, setState] = useState(0);
-    const [sequence, setSequence] = useState([["A"]]);
+    const [sequence, setSequence] = useState([[""]]);
     const [sequenceID, setSequenceID] = useState(-1);
-    const [time, setTime] = useState(1000);
+    const [idsTimeOut,] = useState([]);
+    
 
     const proxSequence = () => {
         const newSequence = [];
-        const newSequenceID = (sequenceID + 1);
-        let newTime = 1000;
+        const newSequenceID = (sequenceID + 1) % 6 ;
         
         switch (newSequenceID) {
             case 0:
-                newTime = 1000;
                 newSequence.push(
                     ["F"], ["E"], ["L"], ["I"], ["Z"], [""], ["N"], ["A"], ["T"], ["A"], ["L"], [""], idLamp, [""]
                 );
                 break;
+            case 1:
+                newSequence.push(
+                    idLamp.filter((_,index) => index % 2 === 0),
+                    idLamp.filter((_,index) => index % 2 === 1)
+                );
+                break;
+            case 2:
+                newSequence.push(["D"], ["D","T"] , ["D","T", "I"], ["D","T", "I"], [""]); 
+                break;
+            case 3:
+                newSequence.push([""]);
+                idLamp.forEach( id => newSequence.push([id]));
+                break;
+            case 4:
+                newSequence.push(idLamp);
+                break;
+            case 5:
+            default:
+                newSequence.push([""]);
+                break;
         }
         setState(0);
-        setSequence(newSequence);
         setSequenceID(newSequenceID);
-        setTime(newTime);
-        
+        setSequence(newSequence);
     };
 
     if (sequenceID === -1) {
         proxSequence();
-        return(<div></div>);
+        return(<div>Carregando...</div>);
     }
     
-    setTimeout(() => setState((state + 1) % sequence.length), time);
-    const idLampOn = sequence[state];
+    
+    const idLampOn = sequence[state % sequence.length];
     const idLampOff = idLamp.filter( a => idLampOn.findIndex(b => a === b) === -1);
-    console.log(idLamp);
-    console.log(idLampOn);
-    console.log(idLampOff);
-
+    
+    while(idsTimeOut.length > 0){
+        clearTimeout(idsTimeOut.pop());
+    }
+    const idTimeOut = setTimeout(() => setState((state + 1) % sequence.length), 1000);
+    idsTimeOut.push(idTimeOut);
+    
     return (
         <div>
             <SvgLoader path={lampImg}>
                 {
                     idLampOn.map(id => (
-                        <SvgProxy selector={`#brightnessOfTheLamp_${id}`} opacity="1" />
+                        <SvgProxy selector={`#brightnessOfTheLamp_${id}`} opacity="1" key={id}/>
                     ))
                 }
                 {
                     idLampOff.map(id => (
-                        <SvgProxy selector={`#brightnessOfTheLamp_${id}`} opacity="0" />
+                        <SvgProxy selector={`#brightnessOfTheLamp_${id}`} opacity="0" key={id}/>
                     ))
                 }
+                <SvgProxy selector="#configLamps" onclick={proxSequence}/>
             </SvgLoader>
         </div>
     );
